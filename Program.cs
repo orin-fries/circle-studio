3b3c7d14f16510731be2223d150c076c6fcabe7e
orindee <85454051+orindee@users.noreply.github.com>
2021-09-20 19:34:02 -0400

    commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Circlestudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int radius;
+            double area;
+
+            Console.WriteLine("Enter a radius: ");
+            radius = int.Parse(Console.ReadLine());
+            area = Math.PI * radius * radius;
+
+            Console.WriteLine("The circle's area is: " + Math.Round(area, 2));
+        }
+    }
+}
